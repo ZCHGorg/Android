cc1b36cc526bbbb3e12873fc68f850ac7b16f7e3
shyshka <ww4ka@ukr.net>
2018-04-26 21:13:57 +0300

    alfa release with testnet

--- src/ChargeHub/app/src/main/java/io/charg/chargstation/root/CommonData.java
+++ src/ChargeHub/app/src/main/java/io/charg/chargstation/root/CommonData.java
@@ -23,9 +23,12 @@ public class CommonData {
     /*public static final String ETH_URL = "https://rinkeby.infura.io/Zp6evGImttk7WOe95WcW";
     public static final String SMART_CONTRACT_ADDRESS = "0x9E27eBE20976782100caA8d6AE60821162C4D3Cf";*/
 
+    /* ferenc
+    public static final String SMART_CONTRACT_ADDRESS = "0x0030740494631cd406e202fe2b91517c7d689259";*/
+
     //using
-    public static final String ETH_URL = "https://mainnet.infura.io/Zp6evGImttk7WOe95WcW";
-    public static final String SMART_CONTRACT_ADDRESS = "0xC4A86561cb0b7EA1214904f26E6D50FD357C7986";
+    public static final String ETH_URL = "https://rinkeby.infura.io/Zp6evGImttk7WOe95WcW";
+    public static final String SMART_CONTRACT_ADDRESS = "0x0030740494631cd406e202fe2b91517c7d689259";
 
     public static final BigInteger GAS_LIMIT_BIG = BigInteger.valueOf(700000);
 }
