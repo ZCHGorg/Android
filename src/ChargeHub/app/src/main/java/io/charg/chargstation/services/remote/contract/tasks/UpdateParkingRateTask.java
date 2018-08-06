d52c385769a9fdb0e2d8a051369bd93a601a421c
shyshka <ww4ka@ukr.net>
2018-08-06 16:22:54 +0300

    update parking rate task

---
+++ src/ChargeHub/app/src/main/java/io/charg/chargstation/services/remote/contract/tasks/UpdateParkingRateTask.java
@@ -0,0 +1,42 @@
+package io.charg.chargstation.services.remote.contract.tasks;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+public class UpdateParkingRateTask extends ChgAsyncTask<TransactionReceipt> {
+
+    private BigInteger mRate;
+
+    public UpdateParkingRateTask(Activity activity, BigInteger rate) {
+        super(activity);
+        mRate = rate;
+    }
+
+    @Override
+    public void executeAsync() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                invokeOnPrepare();
+
+                try {
+                    TransactionReceipt result = mContract.updateParkingRate(mRate).sendAsync().get();
+                    invokeOnComplete(result);
+                } catch (InterruptedException e) {
+                    invokeOnError(e.getMessage());
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    invokeOnError(e.getMessage());
+                    e.printStackTrace();
+                }
+
+                invokeOnFinish();
+            }
+        });
+    }
+}
