cd552adf90bf7f0aeddcbcf7696ca378e697871d
shyshka <ww4ka@ukr.net>
2018-08-06 16:23:30 +0300

    parkingon implementation

---
+++ src/ChargeHub/app/src/main/java/io/charg/chargstation/services/remote/contract/tasks/ParkingOnTask.java
@@ -0,0 +1,42 @@
+package io.charg.chargstation.services.remote.contract.tasks;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+public class ParkingOnTask extends ChgAsyncTask<TransactionReceipt> {
+
+    private BigInteger mTime;
+    private String mAddress;
+
+    public ParkingOnTask(Activity activity, String address, BigInteger time) {
+        super(activity);
+        mTime = time;
+        mAddress = address;
+    }
+
+    @Override
+    public void executeAsync() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                invokeOnPrepare();
+                try {
+                    TransactionReceipt result = mContract.parkingOn(mAddress, mTime).sendAsync().get();
+                    invokeOnComplete(result);
+                } catch (InterruptedException e) {
+                    invokeOnError(e.getMessage());
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    invokeOnError(e.getMessage());
+                    e.printStackTrace();
+                }
+                invokeOnFinish();
+            }
+        });
+    }
+}
