ed5dbce721774af56f2fa2470602a28cdac9d447
shyshka <ww4ka@ukr.net>
2018-08-19 23:55:59 +0300

    added showqrcode method in dialog helper

--- src/ChargeHub/app/src/main/java/io/charg/chargstation/services/helpers/DialogHelper.java
+++ src/ChargeHub/app/src/main/java/io/charg/chargstation/services/helpers/DialogHelper.java
@@ -1,11 +1,26 @@
 package io.charg.chargstation.services.helpers;
 
+import android.annotation.SuppressLint;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.support.v7.app.AlertDialog;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import io.charg.chargstation.R;
+
+import static android.content.Context.WINDOW_SERVICE;
 
 /**
  * Created by worker on 24.11.2017.
@@ -41,4 +56,50 @@ public class DialogHelper {
                 .setView(view)
                 .create();
     }
+
+    public static void showQrCode(final Context context, final String text) {
+        WindowManager manager = (WindowManager) context.getSystemService(WINDOW_SERVICE);
+        if (manager == null) {
+            return;
+        }
+        Display display = manager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+        int smallerDimension = width < height ? width : height;
+        smallerDimension = smallerDimension * 3 / 4;
+
+        try {
+
+            @SuppressLint("InflateParams")
+            View dlgView = LayoutInflater.from(context).inflate(R.layout.dlg_qr, null);
+
+            ImageView imQr = dlgView.findViewById(R.id.iv_qr);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(text, BarcodeFormat.QR_CODE, smallerDimension, smallerDimension);
+            imQr.setImageBitmap(bitmap);
+
+            TextView tvAddress = dlgView.findViewById(R.id.tv_address);
+            tvAddress.setText(StringHelper.getShortEthAddress(text));
+
+            ImageView btnCopy = dlgView.findViewById(R.id.btn_copy);
+            btnCopy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(text);
+                    Toast.makeText(context, "Your address copied to clipboard", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            new AlertDialog.Builder(context)
+                    .setView(dlgView)
+                    .show();
+
+        } catch (Exception e) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
