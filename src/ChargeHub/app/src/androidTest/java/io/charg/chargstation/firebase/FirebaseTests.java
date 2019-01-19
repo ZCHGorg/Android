bd5e34a70a911a6dfb4e8f3ba815aeec4fe0e8e5
shyshka <ww4ka@ukr.net>
2019-01-19 14:33:04 +0200

    firebase tests

--- src/ChargeHub/app/src/androidTest/java/io/charg/chargstation/firebase/FirebaseTests.java
+++ src/ChargeHub/app/src/androidTest/java/io/charg/chargstation/firebase/FirebaseTests.java
@@ -1,5 +1,7 @@
 package io.charg.chargstation.firebase;
 
+import com.google.firebase.database.DatabaseError;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,9 @@ import java.util.concurrent.TimeUnit;
 import androidx.test.runner.AndroidJUnit4;
 import io.charg.chargstation.models.firebase.StationDto;
 import io.charg.chargstation.root.ICallbackOnComplete;
+import io.charg.chargstation.root.ICallbackOnError;
+import io.charg.chargstation.root.ICallbackOnFinish;
+import io.charg.chargstation.root.ICallbackOnPrepare;
 import io.charg.chargstation.services.remote.firebase.tasks.GetStationDtoTask;
 
 @RunWith(AndroidJUnit4.class)
@@ -25,10 +30,38 @@ public class FirebaseTests {
                 station[0] = result;
             }
         });
+        task.setPrepareCallback(new ICallbackOnPrepare() {
+            @Override
+            public void onPrepare() {
+
+            }
+        });
+        task.setErrorCallback(new ICallbackOnError<DatabaseError>() {
+            @Override
+            public void onError(DatabaseError message) {
+
+            }
+        });
+        task.setFinishCallback(new ICallbackOnFinish() {
+            @Override
+            public void onFinish() {
+
+            }
+        });
         task.executeAsync();
 
         TimeUnit.SECONDS.sleep(3);
 
         Assert.assertNotNull(station[0]);
+
+        task.setFinishCallback(null);
+        task.setErrorCallback(null);
+        task.setPrepareCallback(null);
+        task.setCompleteCallback(null);
+        task.executeAsync();
+
+
     }
+
+
 }
